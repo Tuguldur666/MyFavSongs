39154c684137d3e214d1d98790d1ea2b480dde34
Tuguldur666 <apoxmn@gmail.com>
2023-03-19 15:01:53 +0800

    hh
    hh

--- js/login.js
+++ js/login.js
@@ -1,10 +1,22 @@
+let numAttempts = 0;
+let lastAttemptTime = 0;
+let isLockedOut = false;
+
 function login()
 {
     let mail = document.getElementById('email');
     let pass = document.getElementById('password');
     let error = document.getElementById('h3');
+    const currentTime = new Date().getTime();
+    if (mail.value=="mandakh@gmail.edu.mn" && pass.value=="12345678" && numAttempts >= 3 && (currentTime - lastAttemptTime) < 15000) {
+      alert("You have been locked out. Please try again later.");
+      location.href="#"
+      return;
+    }
     if(mail.value=="mandakh@gmail.edu.mn" && pass.value=="12345678")
-    {     location.href="main.html"
+    {    numAttempts = 0;
+        lastAttemptTime = 0;
+        location.href="main.html"
        
        
     }else if(mail.value=="" && pass.value=="")
@@ -14,6 +26,8 @@ function login()
         mail.value=" "
         pass.value=" "
     }else{
+        numAttempts++;
+        lastAttemptTime = currentTime;
         mail.value=""
         pass.value=""
         error.style.display="block"
